422f1f97cbd7cc1ec441a9148faf286a560a34af
Florian Lüscher <florian@florianluescher.ch>
2019-02-27 18:20:42 +0100

    Add metrics

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -1,13 +1,26 @@
 package hello;
 
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
-    
+
+    private final Counter greetingsServed;
+
+    public HelloController(MeterRegistry registry) {
+        this.greetingsServed = registry.counter("greetings.served");
+    }
+
     @RequestMapping("/")
+    @Timed(value="time.greetings")
     public String index() {
+
+        this.greetingsServed.increment();
+
         return "Greetings from Spring Boot, Sunrise!";
     }
     
